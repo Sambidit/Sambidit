83009258c2b4345413b04d05c323d9549deb028e
Sambidit Patnaik <137115003+Sambidit@users.noreply.github.com>
2023-07-02 14:06:20 +0530

    areaofcircle.cpp

--- areaofcircle.cpp
+++ areaofcircle.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
+
+    //Area of circle
     float r;
     cout<<"Radius of circle : ";
     cin>>r;
     cout<<"Area of circle : "<<3.142857142857143*r*r;
+
+    // Circumferance of circle
+    // float r;
+    // cout<<"Radius of circle : ";
+    // cin>>r;
+    // cout<<"Circumferance : "<<2*3.142857142857143*r;
+}
 }
